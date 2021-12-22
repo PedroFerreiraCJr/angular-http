e6e1e7253b524944127eb384173a9cbba0efb48f
Pedro <pedroferreiracjr@gmail.com>
2021-12-22 14:47:58 -0300

    adição de lib ngx-bootstrap; criação de rotas, e componentes iniciais do app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
